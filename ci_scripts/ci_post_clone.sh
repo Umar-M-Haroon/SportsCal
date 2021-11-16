9dfbffa5cec633df3ca166ad90286fe4cc410527
Umar Haroon <umar.m.haroon@gmail.com>
2021-11-15 17:45:34 -0700

    fix script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,4 +6,4 @@
 #  Created by Umar Haroon on 11/15/21.
 #  
 pwd
-printf "import Foundation\nstruct Constants {\n	   static let revenueCatAPIKey = %s\n}" $REVENUECAT_API_KEY > ../Shared/Constants.swift
+printf "import Foundation\nstruct Constants {\n	   static let revenueCatAPIKey = "%s"\n}" $REVENUECAT_API_KEY > ../Shared/Constants.swift
