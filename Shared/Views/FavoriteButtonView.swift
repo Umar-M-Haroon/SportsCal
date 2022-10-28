e1af45218699a34129abba41872f746b68817ba2
Umar Haroon <umar.m.haroon@gmail.com>
2022-10-28 14:46:47 -0600

    refactoring views into smaller views, starting with buttons

--- Shared/Views/FavoriteButtonView.swift
+++ Shared/Views/FavoriteButtonView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-
 import SportsCalModel
 struct FavoriteMenu: View {
     var game: Game?
@@ -16,41 +15,39 @@ struct FavoriteMenu: View {
             Button {
                 if let game = game {
                     if favorites.contains(game) {
-                        favorites.remove(game.home)
+                        favorites.remove(game.strHomeTeam)
                     } else {
-                        favorites.add(game.home)
+                        favorites.add(game.strHomeTeam)
                     }
                 }
             } label: {
                 if let game = game {
-                    if favorites.containsHome(game.home) {
-                        Text("Remove \(game.home) as favorite")
+                    if favorites.containsHome(game.strHomeTeam) {
+                        Text("Remove \(game.strHomeTeam) as favorite")
                     } else {
-                        Text("Set \(game.home) as favorite")
+                        Text("Set \(game.strHomeTeam) as favorite")
                     }
                 }
             }
-            if game?.sport != .F1 {
                 Button {
                     if let game = game {
                         if favorites.contains(game) {
-                            favorites.remove(game.away)
+                            favorites.remove(game.strAwayTeam)
                         } else {
-                            favorites.add(game.away)
+                            favorites.add(game.strAwayTeam)
                         }
                     }
                 } label: {
                     if let game = game {
-                        if favorites.containsAway(game.away) {
-                            Text("Remove \(game.away) as favorite")
+                        if favorites.containsAway(game.strAwayTeam) {
+                            Text("Remove \(game.strAwayTeam) as favorite")
                         } else {
-                            Text("Set \(game.away) as favorite")
+                            Text("Set \(game.strAwayTeam) as favorite")
                         }
                     } else {
                         Text("unsure path")
                     }
                 }
-            }
         } label: {
             if let game = game {
                 if favorites.contains(game) {
---
+++ Shared/Views/NotifyButton.swift
@@ -0,0 +1,56 @@
+//
+//  NotifyButton.swift
+//  SportsCal (iOS)
+//
+//  Created by Umar Haroon on 10/27/22.
+//
+
+import SwiftUI
+import EventKitUI
+import SportsCalModel
+
+struct NotifyButton: View {
+    @Binding var shouldShowSportsCalProAlert: Bool
+    @Binding var sheetType: SheetType?
+    var game: Game
+    var body: some View {
+        Menu {
+            Button {
+                if SubscriptionManager.shared.subscriptionStatus == .notSubscribed {
+                    shouldShowSportsCalProAlert = true
+                } else {
+                    //                                        NotificationManager.addLocalNotification(date: game.gameDate, item: game, duration: .thirtyMinutes)
+                }
+            } label: {
+                Text("\(NotificationDuration.thirtyMinutes.rawValue) before")
+            }
+            Button {
+                if SubscriptionManager.shared.subscriptionStatus == .notSubscribed {
+                    shouldShowSportsCalProAlert = true
+                } else {
+                    //                                        NotificationManager.addLocalNotification(date: game.gameDate, item: game, duration: .oneHour)
+                }
+            } label: {
+                Text("\(NotificationDuration.oneHour.rawValue) before")
+            }
+            Button {
+                
+                if SubscriptionManager.shared.subscriptionStatus == .notSubscribed {
+                    shouldShowSportsCalProAlert = true
+                } else {
+                    //                                        NotificationManager.addLocalNotification(date: game.gameDate, item: game, duration: .twoHour)
+                }
+            } label: {
+                Text("\(NotificationDuration.twoHour.rawValue) before")
+            }
+        } label: {
+            Text("Notify me")
+        }
+    }
+}
+
+//struct NotifyButton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NotifyButton()
+//    }
+//}
