946172e95826fb631a53139185ba7bb36647b5ca
Umar Haroon <umar.m.haroon@gmail.com>
2022-12-21 23:55:14 -0700

    improve reliability of games showing up correctly

--- Shared/Model/GameViewModel.swift
+++ Shared/Model/GameViewModel.swift
@@ -252,7 +252,7 @@ import ActivityKit
                 guard let date = game.isoDate else { return false }
                 if appStorage.hidePastEvents {
 //                    get the date components for the game and check it is greater than 0
-                    return !game.isDone
+                    return !game.hasDoneStatus && Calendar.current.dateComponents([.day], from: .now, to: date).day ?? 0 >= -1 
                 } else {
                     
                     var isValidForPastDuration: Bool = false
