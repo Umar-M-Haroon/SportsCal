bb1074c957a954f941d96834b54c8dbd954f57b8
Umar Haroon <umar.m.haroon@gmail.com>
2026-04-21 01:50:21 -0600

    Pipe AdMob keys through CI post-clone script
    Xcode Cloud injects $ADMOB_APP_ID and $ADMOB_NATIVE_AD_UNIT_ID into the
    generated Constants.swift alongside the existing RevenueCat and API
    keys so the ads initialization picks them up at runtime.

--- SportsCal/ci_scripts/ci_post_clone.sh
+++ SportsCal/ci_scripts/ci_post_clone.sh
@@ -6,5 +6,5 @@
 #  Created by Umar Haroon on 11/15/21.
 #  
 pwd
-printf "import Foundation\nstruct Constants {\n	   static let revenueCatAPIKey = \"%s\"\n	   static let apiKey = \"%s\"\n}" $REVENUECAT_API_KEY $SPORTSCAL_API_KEY > ../Shared/Constants.swift
+printf "import Foundation\nstruct Constants {\n	   static let revenueCatAPIKey = \"%s\"\n	   static let apiKey = \"%s\"\n	   static let adMobAppID = \"%s\"\n	   static let nativeAdUnitID = \"%s\"\n}" $REVENUECAT_API_KEY $SPORTSCAL_API_KEY $ADMOB_APP_ID $ADMOB_NATIVE_AD_UNIT_ID > ../Shared/Constants.swift
 cat ../Shared/Constants.swift
